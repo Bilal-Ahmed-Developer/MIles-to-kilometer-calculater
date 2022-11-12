950d0d06bdd204f713cf4d0f14b99e35205abf2a
Bilal Ahmed <105882416+Bilal-Ahmed-Developer@users.noreply.github.com>
2022-11-12 14:19:38 +0500

    Add files via upload

---
+++ milestokm.js
@@ -0,0 +1,11 @@
+let userValue=prompt("Miles-to-kilometers converter");
+let userValue2 = Number(userValue)*1.60934;
+if (userValue2 === String&&Boolean) {
+     console.log("Please enter the number")
+}else if (userValue2 === 0){
+     console.log("Please enter the number")
+
+}
+ else {
+     console.log(userValue2)
+}
